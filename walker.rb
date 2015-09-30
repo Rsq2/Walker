82731f544afb43c4535144520008dc31ecd69c24
D.J. Dykes <ddykes2@gmail.com>
2015-09-29 22:29:56 -0700

    Fixed bad file input handling

--- walker.rb
+++ walker.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'capybara'
 require 'csv'
 
+
 Capybara.run_server = false
 Capybara.current_driver = :selenium
 
@@ -9,27 +10,37 @@ class Texas_Ranger
   include Capybara::DSL
 
   def initialize
-    assert_csv()
+    accepted_formats = [".csv"]
+    raise ArgumentError.new("Please enter a .csv file") if accepted_formats.include?(File.extname(ARGV[0])) == false
 
+    @filename = ARGV[0].to_s
     @input = merge_array(ARGV[0])
     @output = CSV.open('directions_data.csv', "w")
-    @dump = CSV.open('failed_addresses.csv', "w") 
+  end
+
+# Reformat Array of Arrays from CSV
+  def merge_array(array_of_arrays)
+    merged = Array.new
+    CSV.foreach(array_of_arrays) do |row|
+      merged << row.join(' ')
+    end
+    return merged
   end
 
 # Main Automation
   def round_house
-    visit 'https://www.google.com/maps/dir/'  
+    visit 'https://www.google.com/maps/dir/'
 
   # The Land of Misfit CSS Selectors
     walk_button = find('div.directions-travel-mode-icon.directions-walk-icon')
     search_button = find('button.searchbutton')
     start = find('#sb_ifc50 > input')
     destination = find('#sb_ifc51 > input')
- 
+
     walk_button.click
     start.set($start_point)
 
-  # Lookup 
+  # Lookup
     @input.each do |address|
       sleep 1
       destination.set(address)
@@ -39,62 +50,42 @@ class Texas_Ranger
       this_entry = [address]
       puts "\nCurrent Entry: #{address}"
 
-    # Output Sequence      
+    # Output Sequence
       found = test_address(this_entry)
       if found == true
         collect_data(this_entry)
         take_screenshot(address)
         @output << this_entry
-
-      else
-        @dump << this_entry
-        next
       end
     end
 
   # Termination
-    puts "\n Scraping for #{ARGV[0]} Complete"
-  end
-
-# Chastize Spelling Errors
-  def assert_csv
-    unless ARGV[0].include? '.csv'
-      raise ArgumentError,
-        "Invalid filetype. Enter a .csv as first argument variable."
-    end
-  end
-  
-# Reformat Array of Arrays from CSV 
-  def merge_array(array_of_arrays)
-    merged = Array.new
-    CSV.foreach(array_of_arrays) do |row|
-      merged << row.join(' ')
-    end
-    return merged
+    puts "\n Scraping for #{@filename} Complete"
   end
 
 # Catch For Bad Addresses
-  # TODO: Refactor to split off error case structure
   def test_address(entry)
-    validity = false
+    entry_exists = false
 
     # Approx. Address
     begin
       if page.has_css?('span.widget-directions-error')
-        puts "\t Non-Critical Error: Precision Failure - Exact Address Not Found"
+        puts "\t Precision Failure - Exact Address Not Found"
         entry << 'Precision Failure - Exact Address Not Found'
         puts "\t---> Continuing to Next Entry\n"
-        return validity
+        return entry_exists = false
       end
-    
+
     # No Address
       click_link 'Details'
       sleep 3
     rescue Capybara::ElementNotFound
-      puts "\tNon-Critical Error: Lookup Failure - Address Not Found"
+      puts "\tLookup Failure - Address Not Found"
       entry << 'Lookup Failure - Address Not Found'
       puts "\t---> Continuing to Next Entry\n"
-      return validity
+      return entry_exists = false
+    else
+      return entry_exists = true
     end
 
   # Address Found
@@ -106,7 +97,7 @@ class Texas_Ranger
   def collect_data(entry)
     steps = all('div.numbered-step-content')
     distances =  all('div.directions-mode-distance-time')
-  
+
   # Pair Array Indices 1:1
     steps.zip(distances).each do |step, distance|
       sleep 1
